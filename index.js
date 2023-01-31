da88b7e5e15baae8bbe841fbcd61093052bf3003
Matthew Page <matthew.page@bbc.co.uk>
2023-01-31 14:56:31 +0000

    parses amixer scontents

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
 const { exec } = require('child_process');
 
-play_devices();
-capture_devices();
+//play_devices();
+//capture_devices();
+
+get_mixerControls(1);
+
+async function get_mixerControls(card) {
+    const result = await (execute('amixer', `-c ${card} scontents`));
+    console.log(parseMixer(result));
+}
+
+function parseMixer(data) {
+    const lines = data.split('\n');
+    let controls = [];
+    let control = null;
+
+    lines.forEach((line) => {
+        if (line.match(/^Simple mixer control/)) {
+            if (control) {
+                if (control) controls.push(control);
+                control = null;
+            }
+
+            const name = line.match(/'(.*?)'/)[1];
+            control = {
+                name,
+                state: []
+            };
+        }
+
+        let channels = line.match(/(?<= channels: ).*/);
+        if (channels) {
+            control.channels = channels[0].split(' - ');
+        }
+
+        let limits = line.match(/(?<= Limits: ).*/);
+        if (limits) {
+            const parts = limits[0].split(' ');
+            if (parts[0] == 'Playback') control.playback = true;
+            if (parts[0] == 'Capture') control.capture = true;
+            control.min = parts[1];
+            control.max = parts[3];
+        }
+
+        if (control.channels) {
+            control.channels.forEach((channelName) => {
+                let pattern = new RegExp(`(?<= ${channelName}: ).*`, 'g');
+                let state = line.match(pattern);
+                if (state) {
+                    const parts = state[0].split(' ');
+                    const status = {
+                        channel: channelName,
+                        type: parts[0],
+                        value: parts[1],
+                        percentage: parts[2],
+                        dB: parts[3],
+                        switch: parts[4]
+                    };
+                    control.state = status;
+                }
+            })
+        }
+    });
+
+    if (control) controls.push(control);
+    return controls;
+}
 
 async function play_devices() {
     const result = await execute('aplay', '-l');
